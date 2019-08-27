ac83fb2420a87f1a7fcfc0aca20405b3d12e18fc
Marcel Wira <marcelwira20@gmail.com>
2019-08-27 14:51:23 +0700

    baru mulai buka

--- CancerForKids-Master/Character/CharacterViewController.swift
+++ CancerForKids-Master/Character/CharacterViewController.swift
@@ -27,7 +27,7 @@ class CharacterViewController: UIViewController {
 
     @IBAction func enterNameButton(_ sender: Any) {
         let charName = "\(roundTF.text!)"
-        print(charName)
+        
         //user default charname
     }
 }

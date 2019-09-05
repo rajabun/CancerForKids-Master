98dbebc6d1ec857452c8a815c75075f9a9476cf7
Marcel Wira <marcelwira20@gmail.com>
2019-09-05 16:03:33 +0700

    gatau ini nambah apa

--- CancerForKids-Master/DaftarKegiatan/WaktunyaViewController.swift
+++ CancerForKids-Master/DaftarKegiatan/WaktunyaViewController.swift
@@ -13,7 +13,6 @@ class WaktunyaViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
         // Do any additional setup after loading the view.
     }
     
